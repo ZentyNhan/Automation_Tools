3e85cea80318c107f407ad18a0480eb87bd7a370
ZentyNhan <Wayne.j.N19@gmail.com>
2023-03-12 18:28:55 +0700

    update: Pandas Mar 12th 2023

---
+++ _prj_Testing/Pandas_Test.py
@@ -0,0 +1,11 @@
+import pandas as PD_Test
+
+test_dict = {
+            'P1' : [1.1,1.2,1.3,1.4],
+            'P2' : [2.1,2.2,2.3,2.4],
+            'P3' : [3.1,3.2,3.3,3.4]
+             }
+
+a = PD_Test.DataFrame(test_dict)
+
+print(a)
