5c3e5b9e8baee41fe7f62a6f2cb45758f59cf5f8
ZentyNhan <wayne.j.n19@gmail.com>
2023-03-20 00:51:26 +0700

    Update

--- _prj_Testing/Pandas_Test.py
+++ _prj_Testing/Pandas_Test.py
@@ -1,5 +1,5 @@
 import pandas as PD_Test
-
+import os
 test_dict = {
             'P1' : ['P1.1', 'P1.2', 'P1.3', 'P1.4'],
             'P2' : ['P2.1', 'P2.2', 'P2.3', 'P2.4'],
@@ -37,8 +37,28 @@ i = a.loc[[0,1],['P1','P2']] #[hàng, cột], cột phải là tên keys
 
 
 # ANCHOR - xlsx:
+cur_dir = os.getcwd()
+# print(cur_dir)
+xlsx = PD_Test.read_excel('C:\Test.xlsx', sheet_name='Diem')
+# print(xlsx)
+# print(xlsx.shape)
+# print(xlsx.columns)
+# print(xlsx.columns.ravel()) #set all column name to a list
+# print(xlsx['Long'].tolist()) #set a specific column to a list
+# print(xlsx['Long'].value_counts())
+# print(xlsx.loc[[0,2],'Diễm'])
 
-xlsx = PD_Test.read_excel('Test.xlsx', sheet_name='Diem')
+'''We can specify the column names to be read from the excel file. It’s useful when you are interested in only a few of the columns of the excel sheet'''
+xlsx_specific = PD_Test.read_excel('C:\Test.xlsx', sheet_name='Diem', usecols=['Nhân', 'Diễm', 'Mai'])
+# print(xlsx_specific)
 
-print(xlsx)
+'''If the excel sheet doesn’t have any header row, pass the header parameter value as None.'''
+xlsx_without_header = PD_Test.read_excel('C:\Test.xlsx', sheet_name='Diem', header=None)
+# print(xlsx_without_header)
+
+'''The DataFrame object has various utility methods to convert the tabular data into Dict, CSV, or JSON format.'''
+xlsx_to_dict = PD_Test.read_excel('C:\Test.xlsx', sheet_name='Diem', usecols=['Nhân', 'Diễm'])
+print('Excel Sheet to Dict:', xlsx_to_dict.to_dict(orient='record'))
+print('Excel Sheet to JSON:', xlsx_to_dict.to_json(orient='records'))
+print('Excel Sheet to CSV:\n', xlsx_to_dict.to_csv(index=False))
 
